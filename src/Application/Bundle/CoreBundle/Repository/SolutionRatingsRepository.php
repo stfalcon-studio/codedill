a2d98b4ae2dbf40c95e774640c2af5c7b9de5c17
Andrey Shkodyak <fluxuator@gmail.com>
2014-11-07 16:42:25 +0200

    Added solution ratings table

---
+++ src/Application/Bundle/CoreBundle/Repository/SolutionRatingsRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Application\Bundle\CoreBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query;
+
+/**
+ * SolutionRatingsRepository
+ */
+class SolutionRatingsRepository extends EntityRepository
+{
+}
