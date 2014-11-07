e255a32af842198f44c50410cc52402bad1ea256
Timur Bolotyuh <timur.bolotyuh@gmail.com>
2014-11-07 15:04:01 +0200

    Change controller

--- src/Application/Bundle/CoreBundle/Controller/DefaultController.php
+++ src/Application/Bundle/CoreBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ApplicationCoreBundle:Default:index.html.twig');
+        //TODO dummy
     }
 }
