640a5a2da5c7ea4a420c6a62d18647f85860981a
vatseek <vatseek13@rambler.ru>
2014-11-07 17:29:48 +0200

    add js to send rating

--- src/Application/Bundle/CoreBundle/Resources/public/js/rating.js
+++ src/Application/Bundle/CoreBundle/Resources/public/js/rating.js
@@ -1,1 +1,7 @@
-alert('123');
+$(document).ready(function() {
+    $('#rating-form input[name="rating"]').change(function(e){
+        var form = $('#rating-form');
+        var url = form .prop('action');
+        $.post(url, form.serialize());
+    });
+});
