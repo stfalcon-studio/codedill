712a94499a2c0e57955488a100522c8ef4114672
Andrey Shkodyak <fluxuator@gmail.com>
2014-11-07 13:29:13 +0200

    Fixed user entity. Added repository class

--- src/Application/Bundle/UserBundle/Entity/User.php
+++ src/Application/Bundle/UserBundle/Entity/User.php
@@ -1,16 +1,19 @@
 <?php
 namespace Application\Bundle\UserBundle\Entity;
 
-use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
 use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 /**
- * @ORM\Entity
  * @ORM\Table(name="users")
+ * @ORM\Entity(repositoryClass="Application\Bundle\UserBundle\Repository\UserRepository")
  */
 class User extends BaseUser
 {
+    use TimestampableEntity;
+
     /**
      * @ORM\Id
      * @ORM\Column(type="integer")
@@ -20,44 +23,16 @@ class User extends BaseUser
 
     /**
      * @var string
-     * @ORM\Column(name="ginhubId", type="string", length=255)
+     * @ORM\Column(name="github_id", type="string", length=255)
      */
     protected $githubId;
 
     /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="created_at", type="datetime")
-     * @Gedmo\Timestampable(on="create")
+     * @return int
      */
-    protected $createdAt;
-
-    /**
-     * @var \DateTime
-     *
-     * @ORM\Column(name="updated_at", type="datetime")
-     * @Gedmo\Timestampable(on="update")
-     */
-    protected $updatedAt;
-
-    /**
-     * @return \DateTime
-     */
-    public function getCreatedAt()
+    public function getId()
     {
-        return $this->createdAt;
-    }
-
-    /**
-     * @param \DateTime $createdAt
-     *
-     * @return User
-     */
-    public function setCreatedAt($createdAt)
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
+        return $this->id;
     }
 
     /**
@@ -70,53 +45,9 @@ class User extends BaseUser
 
     /**
      * @param string $githubId
-     *
-     * @return User
      */
     public function setGithubId($githubId)
     {
         $this->githubId = $githubId;
-
-        return $this;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param mixed $id
-     *
-     * @return User
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
-
-        return $this;
-    }
-
-    /**
-     * @return \DateTime
-     */
-    public function getUpdatedAt()
-    {
-        return $this->updatedAt;
-    }
-
-    /**
-     * @param \DateTime $updatedAt
-     *
-     * @return User
-     */
-    public function setUpdatedAt($updatedAt)
-    {
-        $this->updatedAt = $updatedAt;
-
-        return $this;
     }
 }
---
+++ src/Application/Bundle/UserBundle/Repository/UserRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Application\Bundle\CoreBundle\Repository;
+
+use Doctrine\ORM\Query;
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * UserRepository
+ */
+class UserRepository extends EntityRepository
+{
+}
