a462740cf0975cbd4e2f34bc21263cbd232ad17c
Timur Bolotyuh <timur.bolotyuh@gmail.com>
2014-11-07 15:14:12 +0200

    Add feature task list

--- src/Application/Bundle/CoreBundle/Controller/TaskController.php
+++ src/Application/Bundle/CoreBundle/Controller/TaskController.php
@@ -6,8 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class TaskController extends Controller
 {
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction()
     {
-        return $this->render('ApplicationCoreBundle:Task:index.html.twig');
+        $tasks = $this->getDoctrine()->getRepository('ApplicationCoreBundle:Task')->findAll();
+
+        return $this->render(
+            'ApplicationCoreBundle:Task:index.html.twig',
+            [
+                'tasks' => $tasks
+            ]
+        );
     }
 }
