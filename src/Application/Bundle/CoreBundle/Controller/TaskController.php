a8b5d849921d45f847d989cd4411aad248c54de7
Artem Genvald <genvaldartem@gmail.com>
2014-11-07 15:20:01 +0200

    Update add solution action

--- src/Application/Bundle/CoreBundle/Controller/TaskController.php
+++ src/Application/Bundle/CoreBundle/Controller/TaskController.php
@@ -2,9 +2,11 @@
 
 namespace Application\Bundle\CoreBundle\Controller;
 
+use Application\Bundle\CoreBundle\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 /**
  * TaskController
@@ -38,12 +40,15 @@ class TaskController extends Controller
     /**
      * Add solution to task
      *
+     * @param Task $task Task
+     *
      * @return \Symfony\Component\HttpFoundation\Response
      *
      * @Route("/add-solution", name="add_solution")
+     * @ParamConverter("task", class="ApplicationCoreBundle:Task")
      * @Method({"GET"})
      */
-    public function addSolutionAction()
+    public function addSolutionAction(Task $task)
     {
         return $this->render('ApplicationCoreBundle:Task:add_solution.html.twig');
     }
