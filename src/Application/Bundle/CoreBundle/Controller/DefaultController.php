e7ca3b4cf4e10fe2797d5fdbf389fa9e5d80e18a
Timur Bolotyuh <timur.bolotyuh@gmail.com>
2014-11-07 15:34:49 +0200

    Change on homepage

--- src/Application/Bundle/CoreBundle/Controller/DefaultController.php
+++ src/Application/Bundle/CoreBundle/Controller/DefaultController.php
@@ -3,11 +3,20 @@
 namespace Application\Bundle\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+ * DefaultController
+ */
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/", name="homepage")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction()
     {
-        //TODO dummy
+        return $this->render('ApplicationCoreBundle:Default:index.html.twig');
     }
 }
