87159a8e8c3fe2a6202b7495ed62db085fc9db46
Olexandr Lensky <olexandr.lensky@stfalcon.com>
2014-11-07 17:12:53 +0200

    Added solutions list action

--- src/Application/Bundle/CoreBundle/Controller/TaskController.php
+++ src/Application/Bundle/CoreBundle/Controller/TaskController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 /**
  * TaskController
@@ -54,6 +55,36 @@ class TaskController extends Controller
             'ApplicationCoreBundle:Task:add_solution.html.twig',
             [
                 'task' => $task
+            ]
+        );
+    }
+
+    /**
+     * List of solutions to task
+     *
+     * @param Task $task Task
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @Route("/{id}/solutions", name="list_solution")
+     * @ParamConverter("task", class="ApplicationCoreBundle:Task")
+     * @Method({"GET"})
+     */
+    public function listSolutionsAction(Task $task)
+    {
+        $user = $this->getUser();
+        $solutionsRepository = $this->getDoctrine()->getManager()->getRepository('ApplicationCoreBundle:Solution');
+        $userSolutionsForTask = $solutionsRepository->findBy(['user' => $user, 'task' => $task]);
+        if (empty($userSolutionsForTask)) {
+            throw new AccessDeniedHttpException('You must first post your solution');
+        }
+
+        $taskSolutions = $solutionsRepository->findBy(['task' => $task], ['createdAt' => 'DESC']);
+
+        return $this->render(
+            'ApplicationCoreBundle:Task:list_solutions.html.twig',
+            [
+                'solutions' => $taskSolutions
             ]
         );
     }
