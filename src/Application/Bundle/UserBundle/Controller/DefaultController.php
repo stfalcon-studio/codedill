7fa6bfe10e71842288bcab1c7ff026ae9eebb078
Andrey Shkodyak <fluxuator@gmail.com>
2014-11-07 14:35:26 +0200

    Added redirection to auth page. Fixed oauth config

--- src/Application/Bundle/UserBundle/Controller/DefaultController.php
+++ src/Application/Bundle/UserBundle/Controller/DefaultController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($name = 'Головний секретний УКРОП')
     {
-        return $this->render('ApplicationUserBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('ApplicationUserBundle:Default:index.html.twig', ['name' => $name]);
     }
 }
