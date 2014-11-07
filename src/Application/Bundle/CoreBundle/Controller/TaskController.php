d2103045c248e75549d2b97c3c1284a5ea62184b
Artem Genvald <genvaldartem@gmail.com>
2014-11-07 15:13:20 +0200

    Update routing

--- src/Application/Bundle/CoreBundle/Controller/TaskController.php
+++ src/Application/Bundle/CoreBundle/Controller/TaskController.php
@@ -3,11 +3,39 @@
 namespace Application\Bundle\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
+/**
+ * TaskController
+ *
+ * @Route("/")
+ */
 class TaskController extends Controller
 {
+    /**
+     * Index page
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @Route("/", name="index")
+     * @Method({"GET"})
+     */
     public function indexAction()
     {
         return $this->render('ApplicationCoreBundle:Task:index.html.twig');
+    }
+
+    /**
+     * Add solution to task
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @Route("/add-solution", name="add_solution")
+     * @Method({"GET"})
+     */
+    public function addSolutionAction()
+    {
+        return $this->render('ApplicationCoreBundle:Task:add_solution.html.twig');
     }
 }
