269e239b170dbc8eee306f31b8331319995df649
Olexandr Lensky <olexandr.lensky@stfalcon.com>
2014-11-07 12:04:14 +0200

    base config of oauth bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -3,19 +3,11 @@
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
-/**
- * AppKernel
- */
 class AppKernel extends Kernel
 {
-    /**
-     * Register bundles
-     *
-     * @return array|\Symfony\Component\HttpKernel\Bundle\BundleInterface[]
-     */
     public function registerBundles()
     {
-        $bundles = [
+        $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
@@ -25,32 +17,21 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Application\Bundle\CoreBundle\ApplicationCoreBundle(),
-            new Sp\BowerBundle\SpBowerBundle(),
-            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
-            //comment bundle
-            new FOS\RestBundle\FOSRestBundle(),
-            new FOS\CommentBundle\FOSCommentBundle(),
-            new JMS\SerializerBundle\JMSSerializerBundle($this),
-        ];
 
-        if (in_array($this->getEnvironment(), ['dev', 'test'])) {
+            new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
+        );
+
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
         return $bundles;
     }
 
-    /**
-     * Register container configuration
-     *
-     * @param LoaderInterface $loader Loader
-     */
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
     }
 }
