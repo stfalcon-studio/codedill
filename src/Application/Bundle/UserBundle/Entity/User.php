f9d8c06f6462fc3090a929a46d992f882998fecf
Olexandr Lensky <olexandr.lensky@stfalcon.com>
2014-11-07 12:59:27 +0200

    config oauth with fos user bundle

---
+++ src/Application/Bundle/UserBundle/Entity/User.php
@@ -0,0 +1,41 @@
+<?php
+namespace Application\Bundle\UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     * @ORM\Column(name="ginhubId", type="string", length=255)
+     */
+    protected $githubId;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     * @Gedmo\Timestampable(on="create")
+     */
+    protected $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime")
+     * @Gedmo\Timestampable(on="update")
+     */
+    protected $updatedAt;
+}
