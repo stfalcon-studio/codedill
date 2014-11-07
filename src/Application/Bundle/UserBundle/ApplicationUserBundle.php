f808a3063da064063462b7528f51a0e8b51c7d06
Olexandr Lensky <olexandr.lensky@stfalcon.com>
2014-11-07 15:47:07 +0200

    Remove unused files

--- src/Application/Bundle/UserBundle/ApplicationUserBundle.php
+++ src/Application/Bundle/UserBundle/ApplicationUserBundle.php
@@ -1,9 +1,11 @@
 <?php
-
 namespace Application\Bundle\UserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class ApplicationUserBundle
+ */
 class ApplicationUserBundle extends Bundle
 {
 }
